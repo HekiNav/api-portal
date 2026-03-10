f642c107168bb8cb66960ec7f00470e06641473b
Unto <developer.hekinav@gmail.com>
2026-03-10 21:16:03 +0200

    users update

--- src/app/manage/users/page.tsx
+++ src/app/manage/users/page.tsx
@@ -1,16 +1,19 @@
 import { getUsers } from "@/app/actions/user"
 import Card from "@/components/card"
+import Searchable from "@/components/searchable"
 
 export default async function UsersManagePage() {
     const users = await getUsers()
     return (
         <div className="p-4">
             <h1 className="text-2xl font-mono text-blue-800">Manage users</h1>
-            {...users.map((u, i) => (
-                <Card small className="w-30!" cardTitle={u.name || u.id} key={i}>
+            <Searchable items={users.map((u, i) => ({
+                content: <Card small className="w-30!" cardTitle={u.name || u.id} key={i}>
 
-                </Card>
-            ))}
+                </Card>,
+                id: u.id,
+                name: u.name || u.id
+            }))}></Searchable>
         </div>
     )
 }
--- src/components/searchable.tsx
+++ src/components/searchable.tsx
@@ -1,11 +1,48 @@
-import { ReactNode } from "react";
+"use client"
+import { JSX, ReactNode, useEffect, useMemo, useState } from "react";
+import MiniSearch from "minisearch"
 
-export interface SearchableProps<K extends object> {
-    items: {
-        content: ReactNode,
-        fields: {
-            [key in keyof K]: string
-        }
-    },
-    
+export interface SearchableProps {
+    items: SearchableItem[]
+
+}
+export type SearchableFieldKey<T extends string> = T extends "content" ? never : T
+export type SearchableItem = {
+    content: JSX.Element,
+    [key: SearchableFieldKey<string>]: string | JSX.Element
+}
+export default function Searchable({ items }: SearchableProps) {
+    const [query, setQuery] = useState<string>("")
+    const [results, setResults] = useState<ReactNode[]>(items.map(i => i.content))
+    const miniSearch = useMemo(() => new MiniSearch<SearchableItem>({
+        fields: Object.keys(items[0]),
+        storeFields: ["content"]
+    }), [items])
+    miniSearch.addAll(items)
+    useEffect(() => {
+        if (query.length < 2) return setResults(items.map(i => i.content))
+        setResults(miniSearch.search(query, {
+            fuzzy: 0.9
+        }).map(i => ((i as unknown) as SearchableItem).content))
+
+    }, [items, miniSearch, query])
+    return (
+        <div>
+            <input
+                placeholder="Search"
+                autoComplete="search"
+                type="search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                className="my-1 border-black border-3 p-1 accent-blue-800"
+            />
+            <div className="flex flex-row flex-wrap gap-2">
+                {...results.map((r, i) => (
+                    <>
+                        {r}
+                    </>
+                ))}
+            </div>
+        </div>
+    )
 }
