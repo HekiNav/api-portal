16d5754147a8b566ce09c2d980723811768e957f
Unto <developer.hekinav@gmail.com>
2026-03-12 17:06:52 +0200

    more edit ui

--- src/app/manage/services/edit.tsx
+++ src/app/manage/services/edit.tsx
@@ -1,120 +1,134 @@
 "use client"
+import { AreYouSureContext } from "@/components/areyousure"
 import Button from "@/components/button"
+import IconItem from "@/components/iconitem"
 import Modal from "@/components/modal"
 import Toggle from "@/components/toggle"
 import { Service } from "@/lib/definitions"
-import { ArrowBackIos, ArrowForwardIos, EditCalendar } from "@nine-thirty-five/material-symbols-react/sharp"
+import { Add, ArrowBackIos, ArrowForwardIos, EditCalendar } from "@nine-thirty-five/material-symbols-react/sharp"
 import dayjs from "dayjs"
-import { useState } from "react"
+import { forwardRef, PropsWithChildren, Ref, useContext, useState } from "react"
 import { DatePicker } from "react-datepicker"
 
-export default function EditServiceModal({ s }: { s: Service | null }) {
+export default forwardRef(function ({ s, children }: { s: Service | null } & PropsWithChildren) {
+
     const [name, setName] = useState(s?.name || "")
     const [description, setDescription] = useState(s?.description || "")
     const [apiUrl, setApiUrl] = useState(s?.apiUrl || "")
     const [docsUrl, setDocsUrl] = useState(s?.docsUrl || "")
     const [depreciation, setDepreciation] = useState(!!s?.depreciationTime)
     const [depreciationTime, setDepreciationTime] = useState(s?.depreciationTime)
+
+    const [visible, setVisible] = useState(false)
+
+    const openAreYouSure = useContext(AreYouSureContext)
+
     return (
-        <Modal open className="bg-white items-start" cardTitle={`Editing ${name || "New service"}`}>
-            <div className="px-4 flex flex-col w-full">
+        <>
+            <Button onClick={() => setVisible(true)}><IconItem icon={{ Icon: Add, className: "h-4" }}>Create</IconItem></Button>
+            {children}
+            <Modal open={visible} className="bg-white items-start" cardTitle={`Editing ${name || "New service"}`}>
+                <div className="px-4 flex flex-col w-full">
 
-                <label htmlFor="serviceName">
-                    Name
-                </label>
-                <input
-                    placeholder="New service"
-                    autoComplete="name"
-                    type="text"
-                    id="serviceName"
-                    name="serviceName"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
-                />
+                    <label htmlFor="serviceName">
+                        Name
+                    </label>
+                    <input
+                        placeholder="New service"
+                        autoComplete="name"
+                        type="text"
+                        id="serviceName"
+                        name="serviceName"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
+                    />
 
-                <label htmlFor="serviceDescription" className="mt-2">
-                    Description
-                </label>
-                <textarea
-                    placeholder="Processes x and does y"
-                    autoComplete="name"
-                    id="serviceDescription"
-                    name="serviceDescription"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
-                />
+                    <label htmlFor="serviceDescription" className="mt-2">
+                        Description
+                    </label>
+                    <textarea
+                        placeholder="Processes x and does y"
+                        autoComplete="name"
+                        id="serviceDescription"
+                        name="serviceDescription"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
+                    />
 
-                <label htmlFor="apiUrl" className="mt-2">
-                    API endpoint
-                </label>
-                <input
-                    placeholder="https://example.com/api/v1/"
-                    autoComplete="url"
-                    type="url"
-                    id="apiUrl"
-                    name="apiUrl"
-                    value={apiUrl}
-                    onChange={(e) => setApiUrl(e.target.value)}
-                    className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
-                />
+                    <label htmlFor="apiUrl" className="mt-2">
+                        API endpoint
+                    </label>
+                    <input
+                        placeholder="https://example.com/api/v1/"
+                        autoComplete="url"
+                        type="url"
+                        id="apiUrl"
+                        name="apiUrl"
+                        value={apiUrl}
+                        onChange={(e) => setApiUrl(e.target.value)}
+                        className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
+                    />
 
-                <label htmlFor="docsUrl" className="mt-2">
-                    Docs url
-                </label>
-                <input
-                    placeholder="https://example.com/api/v1/docs"
-                    autoComplete="url"
-                    type="url"
-                    id="docsUrl"
-                    name="docsUrl"
-                    value={docsUrl}
-                    onChange={(e) => setDocsUrl(e.target.value)}
-                    className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
-                />
+                    <label htmlFor="docsUrl" className="mt-2">
+                        Docs url
+                    </label>
+                    <input
+                        placeholder="https://example.com/api/v1/docs"
+                        autoComplete="url"
+                        type="url"
+                        id="docsUrl"
+                        name="docsUrl"
+                        value={docsUrl}
+                        onChange={(e) => setDocsUrl(e.target.value)}
+                        className="my-1 border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
+                    />
 
-                <label htmlFor="depreciation" className="mt-2">
-                    Depreciated
-                </label>
-                <Toggle state={depreciation} setState={setDepreciation}></Toggle>
+                    <label htmlFor="depreciation" className="mt-2">
+                        Depreciated
+                    </label>
+                    <Toggle state={depreciation} setState={setDepreciation}></Toggle>
 
-                <label htmlFor="depreciationTime" className="mt-2">
-                    Final support date
-                </label>
-                <DatePicker
-                    className="rounded-none! border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
-                    placeholderText="1.1.1970"
-                    dayClassName={() => "rounded-none!"}
-                    calendarClassName="border-3! border-blue-800! rounded-none!"
-                    icon={<EditCalendar></EditCalendar>}
-                    showIcon
-                    renderCustomHeader={({
-                        date,
-                        changeYear,
-                        changeMonth,
-                        decreaseMonth,
-                        increaseMonth,
-                        prevMonthButtonDisabled,
-                        nextMonthButtonDisabled,
-                    }) => (
-                        <div className="w-full border-b-3 border-blue-800 flex justify-between bg-blue-800 items-center">
-                            <Button onClick={decreaseMonth} className="p-0"><ArrowBackIos /></Button>
-                            <span className="text-white font-mono text-lg">
-                                {dayjs(date).format("MMMM YYYY")}
-                            </span>
-                            <Button onClick={increaseMonth} className="p-0"><ArrowForwardIos /></Button>
-                        </div>
-                    )}
-                    dateFormat="d.M.Y"
-                    selected={depreciationTime}
-                    onChange={(date: Date | null) => setDepreciationTime(date)}></DatePicker>
-                <span className="w-full flex gap-2 justify-center mt-2">
-                    <Button className="bg-blue-400!">Cancel</Button>
-                    <Button>Save</Button>
-                </span>
-            </div>
-        </Modal>
+                    <label hidden={!depreciation} htmlFor="depreciationTime" className="mt-2">
+                        Final support date
+                    </label>
+                    <div hidden={!depreciation}>
+                        <DatePicker
+                            className="rounded-none! border-black border-3 p-1 text-blue-800 accent-blue-800 w-full"
+                            placeholderText="1.1.1970"
+                            dayClassName={() => "rounded-none!"}
+                            calendarClassName="border-3! border-blue-800! rounded-none!"
+                            icon={<div className="flex"><EditCalendar></EditCalendar></div>}
+                            showIcon
+                            renderCustomHeader={({
+                                date,
+                                decreaseMonth,
+                                increaseMonth,
+                            }) => (
+                                <div className="w-full border-b-3 border-blue-800 flex justify-between bg-blue-800 items-center">
+                                    <Button onClick={decreaseMonth} className="p-0"><ArrowBackIos /></Button>
+                                    <span className="text-white font-mono text-lg">
+                                        {dayjs(date).format("MMMM YYYY")}
+                                    </span>
+                                    <Button onClick={increaseMonth} className="p-0"><ArrowForwardIos /></Button>
+                                </div>
+                            )}
+                            dateFormat="d.M.Y"
+                            selected={depreciationTime}
+                            onChange={(date: Date | null) => setDepreciationTime(date)}></DatePicker>
+                    </div>
+                    <span className="w-full flex gap-2 justify-center mt-2">
+                        <Button className="bg-blue-400!" onClick={() => openAreYouSure({
+                            body: "Are you sure you want to PERMANENTLY discard these changes?",
+                            cardTitle: "Discard changes?",
+                            onConfirm: () => setVisible(false)
+                        })}>Cancel</Button>
+                        <Button>Save</Button>
+                    </span>
+                </div>
+            </Modal>
+        </>
     )
 
-}
+})
--- src/app/manage/services/page.tsx
+++ src/app/manage/services/page.tsx
@@ -2,7 +2,7 @@ import Searchable from "@/components/searchable"
 import { getServices } from "@/app/actions/service"
 import Toast from "@/components/toast"
 import ServiceCard from "./card"
-import EditServiceModal from "./edit"
+import EditService from "./edit"
 
 export default async function UsersManagePage() {
     const services = await getServices()
@@ -10,16 +10,18 @@ export default async function UsersManagePage() {
         <Toast message="Failed to load" type={"error"}></Toast>
     )
     console.log(services)
+
     return (
         <div className="p-4">
             <h1 className="text-2xl font-mono text-blue-800">Manage services</h1>
-            {services.length ? <Searchable items={services.map((s, i) => ({
-                content: <ServiceCard key={i} s={s}></ServiceCard>,
-                id: s.id,
-                name: s.name,
-                description: s.description
-            }))}></Searchable> : <p>No services</p>}
-            <EditServiceModal s={null}/>
+            <EditService s={null}>
+                {services.length ? <Searchable items={services.map((s, i) => ({
+                    content: <ServiceCard key={i} s={s}></ServiceCard>,
+                    id: s.id,
+                    name: s.name,
+                    description: s.description
+                }))}></Searchable> : <p>No services</p>}
+            </EditService>
         </div>
     )
 }
