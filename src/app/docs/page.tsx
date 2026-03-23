85f198648cf769f21f9e29b6d5ae277f6507c7a2
Unto <developer.hekinav@gmail.com>
2026-03-23 10:38:07 +0200

    api docs escaped

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -9,13 +9,13 @@ export default async function ApiDocsPage() {
             <h2 className="text-2xl mt-2">Endpoints</h2>
             <h3 className="text-lg mt-2 text-blue-800 underline"><Link href={`/api/auth?sid=<service_id>&token=<hekinav_api_token>`}>/auth?sid=&lt;service&gt;&token=&lt;token&gt;</Link></h3>
             <p>
-                Authenticates API Tokens. Use with your external api to check the validity of the user's API token.
+                Authenticates API Tokens. Use with your external api to check the validity of the user&apos;s API token.
             </p>
 
             <h4 className="mt-2 font-bold">Parameters</h4>
             <table className="border-spacing-1 border-separate">
                 <tr><td>sid</td> <td>Unique id of the service to authenticate for.</td></tr>
-                <tr><td>token</td> <td>Token to authenticate. e.g. "hk.eyXXXXXXXX-XXXXXXXXXXXXXXXX"</td></tr>
+                <tr><td>token</td> <td>Token to authenticate. e.g. &quot;hk.eyXXXXXXXX-XXXXXXXXXXXXXXXX&quot;</td></tr>
             </table>
 
             <h4 className="mt-2 font-bold">Return type</h4>
