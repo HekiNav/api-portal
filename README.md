b4ab35a0db7b2ba2e915b97c0e6fe13b6cdd6c17
Unto <developer.hekinav@gmail.com>
2026-03-23 22:16:33 +0200

    readme and front page update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Api portal
+# Hekinav API portal
+API portal for other APIs.
+
+Use at [api-portal.hekinav.dev](https://api-portal.hekinav.dev)
+
+# Features
+- Secure user system
+- Service and application creation and editing
+- API token generation and resetting
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 "use server"
 
+import Card from "@/components/card";
+import { getServices } from "@/lib/public";
+
 export default async function Home() {
+	const services = await getServices()
 	return (
 		<div className="m-4">
 			<h1 className="font-mono text-5xl text-blue-800">
 				Hekinav API Portal
 			</h1>
+			<p>
+				Portal for generating and authenticating API Tokens for Hekinav APIs.
+			</p>
+			<h2 className="font-mono text-2xl text-blue-800 mt-20">
+				APIs
+			</h2>
+			<div className="api-carousel flex overflow-x-scroll gap-4">
+				{...(services || []).map((s, i) => (
+					<Card key={i} small className="w-50!" cardTitle={s.name}>
+						<div className="px-2">
+							{s.description}
+						</div>
+					</Card>
+				))}
+			</div>
 		</div>
 	);
 }
